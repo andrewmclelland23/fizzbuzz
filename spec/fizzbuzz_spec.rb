5a2d07eafc910c53086ef7bf22e2b831a3dc8601
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-02-19 14:28:26 +0000

    Added test for multiples buzzes

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,32 +1,40 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz fizz 3' do
+describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
 
-describe 'fizzbuzz buzz 5' do
+describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 end
 
-describe 'fizzbuzz fizzbuzz 15' do
+describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
 
-describe 'fizzbuzz returns number 4' do
+describe 'fizzbuzz' do
   it 'returns "4" when passed 4' do
     expect(fizzbuzz(4)).to eq '4'
   end
 end
 
-describe 'fizzbuzz fizz 3 then 6' do
+describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3 then 6' do
     expect(fizzbuzz(3)).to eq 'fizz'
     expect(fizzbuzz(6)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 5, 10 and then 20' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(20)).to eq 'buzz'
   end
 end
