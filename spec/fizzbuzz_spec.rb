35e75480724fc42da4c4f204feccb6fb4daeaa92
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-02-19 13:07:30 +0000

    added test for fizz working for multiples of 3 and code passes all tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,32 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz fizz' do
+describe 'fizzbuzz fizz 3' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
 
-describe 'fizzbuzz buzz' do
+describe 'fizzbuzz buzz 5' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 end
 
-describe 'fizzbuzz fizzbuzz' do
+describe 'fizzbuzz fizzbuzz 15' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
 
-describe 'fizzbuzz returns number' do
+describe 'fizzbuzz returns number 4' do
   it 'returns "4" when passed 4' do
     expect(fizzbuzz(4)).to eq '4'
+  end
+end
+
+describe 'fizzbuzz fizz 3 then 6' do
+  it 'returns "fizz" when passed 3 then 6' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
   end
 end
